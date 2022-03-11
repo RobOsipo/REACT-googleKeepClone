a3b6b41e36c3160240db6e66ae310cb37de7eba2
Robert <osipovitchr@gmail.com>
2022-03-11 10:57:10 -0500

    Created Note component

---
+++ src/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Note() {
+    return (
+        <div>
+            <h1>Title goes here</h1>
+            <p>Content goes here</p>
+        </div>
+    )
+}
+
+
+export default Note;
